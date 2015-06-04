b1d3593b2df11e7cb3ad00f8799076b3f3ee803a
Benjamin Hand <gneeshot@gmail.com>
2015-06-04 17:15:22 -0500

    added default msg, has entered the chatroom, to table

--- db/migrate/20150604221354_create_chat_room.rb
+++ db/migrate/20150604221354_create_chat_room.rb
@@ -2,7 +2,7 @@ class CreateChatRoom < ActiveRecord::Migration
   def change
     create_table :chat_rooms do |t|
       t.string :username, null: false, default: 'anonymous'
-      t.string :msg, default: ''
+      t.string :msg, default: 'has entered the chatroom'
       t.string :room, default: 'global'
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150604195231) do
+ActiveRecord::Schema.define(version: 20150604221354) do
 
   create_table "chat_rooms", force: :cascade do |t|
-    t.string   "username",   default: "anonymous", null: false
-    t.string   "msg",        default: ""
+    t.string   "username",   default: "anonymous",                null: false
+    t.string   "msg",        default: "has entered the chatroom"
     t.string   "room",       default: "global"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
   end
 
 end
