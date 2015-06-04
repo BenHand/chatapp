37ab7b3008f4d91b2ba93bd3ccaea585b3fc867d
Benjamin Hand <gneeshot@gmail.com>
2015-06-04 16:43:38 -0500

    changing new user path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   skip_before_action :Invalid_Authenticity_Token
+  skip_before_action :Verify_Authenticity_Token
 end
