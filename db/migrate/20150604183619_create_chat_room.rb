0eb7f29183826a8b1a00c92d5fbae4c8946b2c17
Benjamin Hand <gneeshot@gmail.com>
2015-06-04 13:44:09 -0500

    created chatroom table, generated migration, controller and model

---
+++ db/migrate/20150604183619_create_chat_room.rb
@@ -0,0 +1,11 @@
+class CreateChatRoom < ActiveRecord::Migration
+  def change
+    create_table :chat_rooms do |t|
+      t.string :username, null: false, default: 'anonymous'
+      t.string :msg, default: ''
+      t.string :room, default: 'global'
+
+      t.timestamp null: false
+    end
+  end
+end
